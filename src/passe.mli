aaf9fed48a79a8530ad6d2d2a8b78b99bdff79ca
Syaiful Bahri <syai.bahri6@gmail.com>
2026-05-14 14:49:43 +0700

    fix: use module type definition for Bcrypt and Argon2

--- src/passe.mli
+++ src/passe.mli
@@ -1,4 +1,4 @@
-type hash
+type hash = Hash.t
 (** A hashed password. This represent a password that has been put through
  a hashing function. *)
 
@@ -10,66 +10,8 @@ val hash_of_string : string -> hash
  function must only be used when you know your string is the result of previous
  operation, eg when you get the string from database. *)
 
-module Bcrypt : sig
-  type error =
-    [ `Hash_failure
-    | `Invalid_cost of string
-    | `Invalid_hash of string
-    | `Invalid_prefix of string
-    | `Invalid_salt_length of int
-    | `Salt_generation_failure
-    | `Truncated of int
-    ]
+module Bcrypt = Bcrypt
+(** The bcrypt hashing algorithm. *)
 
-  val pp_error : Format.formatter -> error -> unit
-  (** [pp_error fmt err] pretty print bcrypt error [err] to formatter [fmt] *)
-
-  val hash_with_salt : salt:string -> string -> (hash, error) result
-  (** [hash_with_salt ~salt password] hash the [password] using the given [salt] *)
-
-  val hash : ?cost:int -> string -> (hash, error) result
-  (** [hash ?cost password] hash the [password] using the given [cost].
-      If no cost is provided, a default cost of 12 is used. *)
-
-  val verify : hash:hash -> string -> (bool, error) result
-  (** [verify ~hash password] verify that the [password] matches the given [hash] *)
-end
-
-module Argon2 : sig
-  type error =
-    [ `Invalid_time_cost of string
-    | `Invalid_memory_cost of string
-    | `Invalid_parallelism of string
-    | `Invalid_salt_length of int
-    | `Hash_failure of string
-    | `Verify_mismatch
-    | `Invalid_hash of string
-    ]
-
-  val pp_error : Format.formatter -> error -> unit
-  (** [pp_error fmt err] pretty print bcrypt error [err] to formatter [fmt] *)
-
-  type params =
-    { t_cost : int
-    ; m_cost : int
-    ; parallelism : int
-    }
-
-  val default_params : params
-  (** default parameters following OWASP recommendations *)
-
-  val hash_with_salt :
-     salt:string
-    -> params:params
-    -> string
-    -> (hash, error) result
-  (** [hash_with_salt ~salt ~params password] hash the [password] using the given
-      [salt] and [params] *)
-
-  val hash : ?params:params -> string -> (hash, error) result
-  (** [hash ?params password] hash the [password] using the given [params].
-      If no params is provided, default params are used. *)
-
-  val verify : hash:hash -> string -> (bool, error) result
-  (** [verify ~hash password] verify that the [password] matches the given [hash] *)
-end
+module Argon2 = Argon2
+(** The argon2 hashing algorithm. *)
